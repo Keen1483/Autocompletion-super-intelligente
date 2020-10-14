093a34fb942a04e5d1cd73481093ce41cde3f8d4
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2020-10-14 04:59:03 +0100

    L'autocomplétion super intélligente

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    if(document.getElementById('main')) {
+        $('.autocomplete-region').css('position', 'absolute').css('z-index', 2).css('background', 'transparent');
+        $('.autocomplete-department').css('position', 'absolute').css('z-index', 2).css('background', 'transparent');
+        $('.autocomplete-borough').css('position', 'absolute').css('z-index', 2).css('background', 'transparent');
+    }
+});
